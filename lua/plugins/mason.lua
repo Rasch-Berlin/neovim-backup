0749185dd24113d909f3a1ba46aea96bf1a61501
Ralph Schuengel <ralph.schuengel@gmail.com>
2026-03-08 14:13:48 +0100

    Initial LazyVim config backup

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  -- Wir erweitern die Konfiguration von mason.nvim
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "astro-language-server",
+        "bash-language-server",
+        "codebook",
+        "css-lsp",
+        "docker-language-server",
+        "marksman",
+        "python-lsp-server",
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        "systemd-lsp",
+        "yaml-language-server",
+      },
+    },
+  },
+}
