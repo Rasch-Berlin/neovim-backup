e541aac05fe763171e8266182779853a9671dd69
Ralph Schuengel <ralph.schuengel@gmail.com>
2026-03-08 14:16:14 +0100

    options.lua angepasst für copy and past

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- OSC52 als Clipboard-Provider erzwingen
+if vim.fn.has('nvim-0.10') == 1 then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
+
+-- Nutze das System-Clipboard für alle Yank-Operationen
+vim.opt.clipboard = "unnamedplus"
+
+
